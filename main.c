ae0fd04ffd1fe01c32e4f3b76df2eef12e84f5f5
giuse-pio <giupeppepio7@gmail.com>
2024-10-15 18:10:25 +0200

    con il ciclo for

--- main.c
+++ main.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
 int main(void) {
-    int n=100;
-
-    while(n>=0) {
+    int n=0;
+    for (n=100; n>=0; n=n-2) {
+    printf("%d\n", n);
+}
+    /*while(n>=0) {
         printf("%d\n", n);
         n=n-2;
-    }
+    }*/
 }
